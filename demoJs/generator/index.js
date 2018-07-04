d93b3ae3f0cfa002cb31f11c5ae3207c5045d86f
yanxiaofei <yanxiaofei@xkeshi.com>
2018-07-04 18:34:41 +0800

    feat: 增加 generator 备注

--- demoJs/generator/index.js
+++ demoJs/generator/index.js
@@ -1,32 +1,15 @@
 var fs = require('fs')
-var thunkify = require('thunkify')
+var { thunkify, run } = require('./thunkify')
 var readFileThunk = thunkify(fs.readFile)
 
+// console.log(readFileThunk.toString())
 var gen = function* () {
-  console.log('111')
-  var r1 = yield readFileThunk('./test.html')
-  console.log('222')
-  var r2 = yield readFileThunk('./.gitignore')
-  console.log('333')
-}
-
-
-function run(fn) {
-  // 这里获得genrator后的函数
-  var gen = fn()
-
-  function next(err, data) {
-    // 获取到第一 yield 分段函数值 ({ value: [Function], done: false })
-    var result = gen.next(data)
-
-    // 如果 done 为 true，则表示 yield 全部执行完毕，直接退出
-    if (result.done) return
-
-    // 否则把 next 函数，传给 thunkify模块的 function(done){ ... }
-    result.value(next)
-  }
-
-  next();
+  // console.log('111')
+  var r1 = yield readFileThunk('./test.eee.js')
+  // console.log('r1', r1)
+  var r2 = yield readFileThunk('./test.eee.1.js')
+  // console.log('r2', r2)
+  // console.log('333')
 }
 
 run(gen)
--- demoJs/generator/thunkify.js
+++ demoJs/generator/thunkify.js
@@ -7,7 +7,11 @@
  * Expose `thunkify()`.
  */
 
-module.exports = thunkify;
+module.exports = {
+  thunkify,
+  run,
+};
+
 
 /**
  * Wrap a regular callback `fn` as a thunk.
@@ -17,24 +21,23 @@ module.exports = thunkify;
  * @api public
  */
 
+ // 收集回调 fs.readFile
 function thunkify(fn){
+  // 收集文件名
   return function(){
-    var args = new Array(arguments.length);
+    var args = Array.from(arguments);
     var ctx = this;
-    for(var i = 0; i < args.length; ++i) {
-      args[i] = arguments[i];
-    }
 
+    // done 参数是 run 执行后的next方法
     return function(done){
       var called;
-
       args.push(function(){
         if (called) return;
         called = true;
         done.apply(null, arguments);
       });
-
       try {
+        // args值：['./test.eee.js', function(){...}]
         fn.apply(ctx, args);
       } catch (err) {
         done(err);
@@ -42,3 +45,24 @@ function thunkify(fn){
     }
   }
 };
+
+
+function run(fn) {
+  // 这里获得 generator 后的函数
+  var gen = fn()
+
+  // data : 获取到的文件内容
+  function next(err, data) {
+    console.log(data)
+    // 获取到第一 yield 分段函数值 ({ value: [Function], done: false })
+    var result = gen.next(data)
+
+    // 如果 done 为 true，则表示 yield 全部执行完毕，直接退出
+    if (result.done) return
+
+    // 否则把 next 函数，传给 thunkify模块的 function(done){ ... }
+    result.value(next)
+  }
+
+  next();
+}
